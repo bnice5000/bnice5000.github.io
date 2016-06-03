104f6aafe264494fb4d30f211e86258f34322b4f
Brian Levin <brianlevin@Fluorine.local>
2016-06-03 10:51:28 -0400

    Working on FS App Article.

--- theme/js/bodypadding.js
+++ theme/js/bodypadding.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $('body').css('padding-top', $('.navbar').height()+'px');
 });
+
+var shiftWindow = function() { scrollBy(0, -50) };
+if (location.hash) shiftWindow();
+  window.addEventListener("hashchange", shiftWindow);
