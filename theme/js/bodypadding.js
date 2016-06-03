767a79f27dc5b8b881d9c12ed11423db28111656
Brian Levin <brianlevin@Fluorine.local>
2016-06-03 11:13:22 -0400

    Working on FS App Article.

--- theme/js/bodypadding.js
+++ theme/js/bodypadding.js
@@ -1,16 +1,3 @@
 $(document).ready(function(){
     $('body').css('padding-top', $('.navbar').height()+'px');
 });
-
-$(function() {
-    setTimeout(delayedFragmentTargetOffset, 500);
-});
-
-// add scroll offset to fragment target (if there is one)
-function delayedFragmentTargetOffset(){
-    var offset = $(':target').offset();
-    if(offset){
-        var scrollto = offset.top - 50; // minus fixed header height
-        $('html, body').animate({scrollTop:scrollto}, 0);
-    }
-}
