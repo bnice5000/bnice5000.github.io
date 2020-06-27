86f2b74eeceedabe0990094e32e49ec4b9b9fc6d
Brian Levin <brian.g.levin@gmail.com>
2020-06-27 23:45:27 +0200

    'Publish site on 2020-06-27'

---
+++ theme/js/super_fences.js
@@ -0,0 +1,16 @@
+(function (doc) {
+  function onReady(fn) {
+    if (doc.addEventListener) {
+      doc.addEventListener('DOMContentLoaded', fn);
+    } else {
+      doc.attachEvent('onreadystatechange', function() {
+        if (doc.readyState === 'interactive')
+          fn();
+      });
+    }
+  }
+onReady(function(){
+    convertUML('uml-flowchart', flowchart);
+    convertUML('uml-sequence-diagram', Diagram, {theme: 'simple'});
+  });
+})(document)
