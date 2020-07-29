ca81ef6d94b8cbe48dc45f6a36ee8b54523180ac
Brian Levin <brian.g.levin@gmail.com>
2020-07-29 19:46:39 +0200

    'Publish site on 2020-07-29'

--- theme/js/lunr-search-results.js
+++ theme/js/lunr-search-results.js
@@ -33,7 +33,7 @@ function lunr_search(term) {
       "lunr-search-result-heading"
     );
     const resultIntro = `
-    <h1>Search Results for <code>${term}</code></h1>
+    <h1>Search Results for <pre>${term}</pre></h1>
     `;
 
     resultHeadingRoot.insertAdjacentHTML("beforeend", resultIntro);
